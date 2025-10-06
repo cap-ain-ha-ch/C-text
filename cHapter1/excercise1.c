dd11179266bb496c07705bd032848205b48db8f1
cap-ain-ha-ch <sehalsanthosh@gmail.com>
2025-10-06 20:13:31 +0530

    Initial Commit

---
+++ cHapter1/excercise1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    printf("no slash n");
+    printf("Hello world\n");
+    return 0;
+}
---
+++ cHapter1/excercise2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    printf("no slash n\c");
+    printf("Hello world\n");
+    return 0;
+}
